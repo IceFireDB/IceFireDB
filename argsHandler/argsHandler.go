c05a3e362ec1e7e55b4a531684c46c108220b70b
Jchicode <qianggeek@gmail.com>
2021-08-26 18:30:54 +0800

    fix :update RedisCmdRewrite param

--- argsHandler/argsHandler.go
+++ argsHandler/argsHandler.go
@@ -8,15 +8,12 @@ import (
 
 //rewrite some redis instructions
 
-func RedisCmdRewrite(args [][]string, v ...interface{}) {
-	var nowUnixTime int64
+func RedisCmdRewrite(args [][]string) {
+	nowUnixTime := time.Now()
 
 	for i, arg := range args {
 		switch strings.ToLower(arg[0]) {
 		case "setex":
-			if nowUnixTime == 0 {
-				nowUnixTime = getUnixTime(v...)
-			}
 			if len(arg) == 4 && nowUnixTime != 0 {
 				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
 				if err == nil {
@@ -24,47 +21,27 @@ func RedisCmdRewrite(args [][]string, v ...interface{}) {
 				}
 			}
 		case "expire":
-			if nowUnixTime == 0 {
-				nowUnixTime = getUnixTime(v...)
-			}
 			if len(arg) == 3 && nowUnixTime != 0 {
 				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
 				if err == nil {
-					args[i] = []string{"expireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10), arg[2]}
+					args[i] = []string{"expireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10)}
 				}
 			}
 		case "lexpire":
-			if nowUnixTime == 0 {
-				nowUnixTime = getUnixTime(v...)
-			}
 			if len(arg) == 3 && nowUnixTime != 0 {
 				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
 				if err == nil {
-					args[i] = []string{"lexpireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10), arg[2]}
+					args[i] = []string{"lexpireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10)}
 				}
 			}
 		case "hexpire":
-			if nowUnixTime == 0 {
-				nowUnixTime = getUnixTime(v...)
-			}
 			if len(arg) == 3 && nowUnixTime != 0 {
 				exDuration, err := strconv.ParseInt(arg[2], 10, 64)
 				if err == nil {
-					args[i] = []string{"lexpireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10), arg[2]}
+					args[i] = []string{"lexpireat", arg[1], strconv.FormatInt(nowUnixTime+exDuration, 10)}
 				}
 			}
 		default:
 		}
 	}
-}
-
-func getUnixTime(v ...interface{}) int64 {
-	var nowUnixTime int64
-	for _, vv := range v {
-		switch vtype := vv.(type) {
-		case time.Time:
-			nowUnixTime = vtype.Unix()
-		}
-	}
-	return nowUnixTime
 }
