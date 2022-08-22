05eb7fa3569483092fe59e5c332b11b13fb58123
ShengTao <taosheng@corp-ci.com>
2022-08-22 15:31:52 +0800

    fix pubsub arg bug

--- IceFireDB-PubSub/pkg/router/redisNode/ppubsub.go
+++ IceFireDB-PubSub/pkg/router/redisNode/ppubsub.go
@@ -12,8 +12,19 @@ func (r *Router) cmdPpub(s *router.Context) error {
 		return errors.New("ERR wrong number of arguments for 'ppub' command")
 	}
 
-	topicName := string(args[1].([]byte))
-	message := string(args[2].([]byte))
+	var topicName, message string
+
+	// p2p 过来的数据，这边类型不是 []byte，而是 string，所以要转换一下
+	if args1Byte, ok := args[1].([]byte); ok {
+		topicName = string(args1Byte)
+	} else {
+		topicName = args[1].(string)
+	}
+	if args2Byte, ok := args[2].([]byte); ok {
+		message = string(args2Byte)
+	} else {
+		message = args[2].(string)
+	}
 	err := ppubsub.Pub(topicName, message)
 	if err != nil {
 		return errors.New("ERR pub:" + err.Error())
