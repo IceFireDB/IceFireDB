996774a8f744e1b724c08b5422afb95e0eac0bdf
gitsrc <34047788+gitsrc@users.noreply.github.com>
2024-06-15 22:09:27 +0800

    feat(ci):update ci
    Signed-off-by: gitsrc <34047788+gitsrc@users.noreply.github.com>

--- conn_test.go
+++ conn_test.go
@@ -4,14 +4,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 
 	"github.com/ledisdb/ledisdb/ledis"
 
+	"github.com/cenkalti/backoff/v4"
 	"github.com/go-redis/redis/v8"
 	lediscfg "github.com/ledisdb/ledisdb/config"
 	"github.com/tidwall/uhaha"
@@ -57,13 +60,13 @@ func getTestConn() *redis.Client {
 
 		log.Println("waiting for DB bootstrap")
 		// wait server starts
-		// backoff.Retry(func() error {
-		// 	_, err := testRedisClient.Set(context.Background(), "init", "1", 0).Result()
-		// 	return err
-		// }, backoff.NewConstantBackOff(1*time.Second))
+		backoff.Retry(func() error {
+			_, err := testRedisClient.Set(context.Background(), "init", "1", 0).Result()
+			return err
+		}, backoff.NewConstantBackOff(1*time.Second))
 
-		// // clean all data
-		// testRedisClient.FlushAll(context.Background())
+		// clean all data
+		testRedisClient.FlushAll(context.Background())
 	}
 	testConnOnce.Do(f)
 	return testRedisClient
