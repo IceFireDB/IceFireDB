8b04d63db3ccc8ab0f00a4086ff6d7af7f1f4dbe
xixishidibei <xixishidibei@outlook.com>
2025-04-06 09:30:19 +0800

    refactor: use time.DateTime (#861)
    Signed-off-by: xixishidibei <xixishidibei@outlook.com>

--- IceFireDB-SQLite/utils/util.go
+++ IceFireDB-SQLite/utils/util.go
@@ -27,7 +27,7 @@ func GoWithRecover(handler func(), recoverHandler func(r interface{})) {
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
-				logrus.Errorf("%s goroutine panic: %v\n%s\n", time.Now().Format("2006-01-02 15:04:05"), r, string(debug.Stack()))
+				logrus.Errorf("%s goroutine panic: %v\n%s\n", time.Now().Format(time.DateTime), r, string(debug.Stack()))
 				if recoverHandler != nil {
 					go func() {
 						defer func() {
